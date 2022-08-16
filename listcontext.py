21972d6b981b33711019a5bb4211b974d9df6881
Wolfgang Rosenauer <wr@rosenauer.org>
2022-08-16 23:02:03 +0200

    add "exists" mode to listcontext to allow simple check if a context already exists

--- listcontext.py
+++ listcontext.py
@@ -22,36 +22,56 @@ import settings
 
 def main():
     parser = argparse.ArgumentParser(
-        description='List contexts of an OX Cloud reseller.')
+        description='List (or checks) contexts of an OX Cloud reseller.')
     parser.add_argument("-s", "--searchpattern", help="The search pattern which is used for listing.")
+    parser.add_argument("--exists", help="Check for context existance (use with -c or -n).", action="store_true")
+    parser.add_argument("-c", "--cid", help="Context ID.", type=int)
+    parser.add_argument("-n", dest="context_name", help="Context name.")
     parser.add_argument("--long", help="Verbose output (incl. settings).", action="store_true")
     args = parser.parse_args()
 
     client = Client(settings.getHost()+"OXResellerContextService?wsdl")
 
-    if args.searchpattern is not None:
-        search = "*" + args.searchpattern + "*"
+    if args.exists is True:
+        if args.context_name is None and args.cid is None:
+            parser.error("Context must be specified by either -n or -c !")
+
+        ctx = {}
+        if args.cid is not None:
+            ctx["id"] = args.cid
+        else:
+            ctx["name"] = settings.getCreds()["login"] + "_" + args.context_name
+
+        exists = client.service.exists(ctx, settings.getCreds())
+        # this might throw exceptions which should be handled gracefully (NB: ->exists is currently broken with MWB-1774)
+        if exists is True:
+            print("The context exists!")
+        else:
+            print("The context does not exist!")
     else:
-        search = "*"
+        if args.searchpattern is not None:
+            search = "*" + args.searchpattern + "*"
+        else:
+            search = "*"
 
-    contexts = client.service.list(search, settings.getCreds())
+        contexts = client.service.list(search, settings.getCreds())
 
-    if not args.long:
-        print ("{:<7} {:<40} {:<10}".format('CID', 'Name', 'maxQuota'))
-        for context in contexts:
-            print ("{:<7} {:<40} {:<10}".format(context.id, context.name, str(context.maxQuota)))
-    else:
-        print("{:<7} {:<40} {:<10}".format('CID', 'Name', 'Quota'))
-        for context in contexts:
-            print("{:<7} {:<40} {:<10}".format(
-                context.id, context.name, str(context.usedQuota) + "/" + str(context.maxQuota)))
+        if not args.long:
+            print ("{:<7} {:<40} {:<10}".format('CID', 'Name', 'maxQuota'))
+            for context in contexts:
+                print ("{:<7} {:<40} {:<10}".format(context.id, context.name, str(context.maxQuota)))
+        else:
+            print("{:<7} {:<40} {:<10}".format('CID', 'Name', 'Quota'))
+            for context in contexts:
+                print("{:<7} {:<40} {:<10}".format(
+                    context.id, context.name, str(context.usedQuota) + "/" + str(context.maxQuota)))
 
-            for entries in context.userAttributes.entries:
-                if entries.key == 'config':
-                    print ("Configuration")
-                    for prop in entries.value.entries:
-                        print (prop.key, ":", prop.value)
-                    print ("\n")
+                for entries in context.userAttributes.entries:
+                    if entries.key == 'config':
+                        print ("Configuration")
+                        for prop in entries.value.entries:
+                            print (prop.key, ":", prop.value)
+                        print ("\n")
 
 
 if __name__ == "__main__":
