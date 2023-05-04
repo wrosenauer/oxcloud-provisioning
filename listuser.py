ebd03ff616f046e6bf1430b01f4f30c24ac9fdae
Wolfgang Rosenauer <wr@rosenauer.org>
2023-05-04 11:47:03 +0200

    classofservice is now (correctly again) an array

--- listuser.py
+++ listuser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright (C) 2022  OX Software GmbH
+# Copyright (C) 2023  OX Software GmbH
 #                     Wolfgang Rosenauer
 #
 # This program is free software: you can redistribute it and/or modify
@@ -109,7 +109,7 @@ def main():
                     mailquotaUsage = oxaasService.getQuotaUsagePerUser(
                         ctx.id, user.id, settings.getCreds())
                     print("{:<3} {:<40} {:<30} {:<12} {:<12} {:<15} {:<20} {:<15}".format(
-                        user.id, user.name, user.primaryEmail, str(user.usedQuota) + "/" + str(user.maxQuota), str(round(mailquotaUsage.storage/1024)) + "/" + str(mailquota), str(acn), cos, spamlevel))
+                        user.id, user.name, user.primaryEmail, str(user.usedQuota) + "/" + str(user.maxQuota), str(round(mailquotaUsage.storage/1024)) + "/" + str(mailquota), str(acn), ','.join(cos), spamlevel))
                 else:
                     print("{:<3} {:<40} {:<30} {:<12} {:<12} {:<15} {:<20} {:<15}".format(
                         user.id, user.name, user.primaryEmail, str(user.usedQuota) + "/" + str(user.maxQuota), str(acn), "n/a", "n/a", "n/a"))
