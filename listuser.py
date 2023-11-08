bfcac10b7b5a01210fa5d71e2893771f9678f06b
Wolfgang Rosenauer <wr@rosenauer.org>
2023-11-08 10:18:21 +0100

    deal with potential guest users differently since it's hard to identify them programatically

--- listuser.py
+++ listuser.py
@@ -59,7 +59,8 @@ def main():
         users = userService.listCaseInsensitive(
             ctx, "*"+args.search+"*", settings.getCreds())
     else:
-        users = userService.listAll(ctx, settings.getCreds(), args.includeguests)
+        users = userService.listAll(
+            ctx, settings.getCreds(), args.includeguests)
 
     users = userService.getMultipleData(
         ctx, users, settings.getCreds())
@@ -80,39 +81,41 @@ def main():
 
         if not args.dump:
 
-            if user.primaryEmail is None:
-                print("{:<3} {:<40} ".format(
-                    user.id, user.name))
-            else:
-                if user.id != 2:
-                    if not args.skip_cos:
-                        cos = 'unset'
-                        r = requests.get(settings.getRestHost()+"oxaas/v1/admin/contexts/"+str(
-                            ctx.id)+"/users/"+str(user.id)+"/classofservice", auth=(settings.getRestCreds()), verify=settings.getVerifyTls())
-                        if r.status_code == 200:
-                            if r.json()['classofservice'] != '':
-                                cos = r.json()['classofservice']
+            if user.id != 2:
+                if not args.skip_cos:
+                    cos = "unset"
+                    r = requests.get(settings.getRestHost()+"oxaas/v1/admin/contexts/"+str(
+                        ctx.id)+"/users/"+str(user.id)+"/classofservice", auth=(settings.getRestCreds()), verify=settings.getVerifyTls())
+                    if r.status_code == 200:
+                        if r.json()['classofservice'] != '':
+                            cos = r.json()['classofservice']
 
-                    if not args.skip_acn:
-                        acn = userService.getAccessCombinationName(
-                            ctx, user, settings.getCreds())
+                if not args.skip_acn:
+                    acn = userService.getAccessCombinationName(
+                        ctx, user, settings.getCreds())
 
-                    if not args.skip_spamlevel:
-                        r = requests.get(settings.getRestHost()+"oxaas/v1/admin/contexts/"+str(
-                            ctx.id)+"/users/"+str(user.id)+"/spamlevel", auth=(settings.getRestCreds()), verify=settings.getVerifyTls())
-                        if r.status_code == 200:
-                            if r.json()['spamlevel'] != '':
-                                spamlevel = r.json()['spamlevel']
+                if not args.skip_spamlevel:
+                    r = requests.get(settings.getRestHost()+"oxaas/v1/admin/contexts/"+str(
+                        ctx.id)+"/users/"+str(user.id)+"/spamlevel", auth=(settings.getRestCreds()), verify=settings.getVerifyTls())
+                    if r.status_code == 200:
+                        if r.json()['spamlevel'] != '':
+                            spamlevel = r.json()['spamlevel']
+                try:
+                    # fails for Guest users w/o LDAP entry
                     mailquota = oxaasService.getMailQuota(
                         ctx.id, user.id, settings.getCreds())
                     # can error if Dovecot has no mailbox/quotausage yet OPS-13238
                     mailquotaUsage = oxaasService.getQuotaUsagePerUser(
                         ctx.id, user.id, settings.getCreds())
-                    print("{:<3} {:<40} {:<30} {:<12} {:<12} {:<15} {:<20} {:<15}".format(
-                        user.id, user.name, user.primaryEmail, str(user.usedQuota) + "/" + str(user.maxQuota), str(round(mailquotaUsage.storage/1024)) + "/" + str(mailquota), str(acn), ','.join(cos), spamlevel))
-                else:
-                    print("{:<3} {:<40} {:<30} {:<12} {:<12} {:<15} {:<20} {:<15}".format(
-                        user.id, user.name, user.primaryEmail, str(user.usedQuota) + "/" + str(user.maxQuota), str(acn), "n/a", "n/a", "n/a"))
+                    mailquotaUsage = str(round(mailquotaUsage.storage/1024))
+                except:
+                    mailquota = "-"
+                    mailquotaUsage = "-"
+                print("{:<3} {:<40} {:<30} {:<12} {:<12} {:<15} {:<20} {:<15}".format(
+                    user.id, user.name, user.primaryEmail, str(user.usedQuota) + "/" + str(user.maxQuota), mailquotaUsage + "/" + str(mailquota), str(acn), ','.join(cos), spamlevel))
+            else:
+                print("{:<3} {:<40} {:<30} {:<12} {:<12} {:<15} {:<20} {:<15}".format(
+                    user.id, user.name, user.primaryEmail, str(user.usedQuota) + "/" + str(user.maxQuota), str(acn), "n/a", "n/a", "n/a"))
 
         else:
             print(user)
